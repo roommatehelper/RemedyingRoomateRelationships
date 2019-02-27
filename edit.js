13603e831de203e14ca7861fafee94bf229ac946
Daniella <daniellaa00@gmail.com>
2019-02-27 10:32:24 -0600

    Add Rabin's Edits

---
+++ edit.js
@@ -0,0 +1,78 @@
+function addMember() {
+  var elements = document.getElementById("newMemberForm").elements;
+  var obj ={};
+  for(var i = 0 ; i < elements.length ; i++){
+      var item = elements.item(i);
+      obj[item.name] = item.value;
+  }
+  var line = document.createElement("div");
+  line.className += "line";
+  var rule = document.createElement("button");
+  rule.className += "ruleTitle";
+  rule.type = "button";
+  rule.innerHTML += obj["membername"];
+  line.appendChild(rule);
+  var etc = document.createElement("div");
+  etc.className += "ruleDescription";
+  var desc = document.createElement("p");
+  var details = document.createElement("p");
+  var del = document.createElement("input");
+    del.setAttribute("type", "button");
+    del.setAttribute("class", "descButton delete");
+    del.setAttribute("value", "Uninvite");
+    del.setAttribute("onclick", "removeMember(this)");
+  details.innerHTML += "Invite Pending";
+  etc.appendChild(details);
+  etc.append(del);
+
+  line.appendChild(etc);
+
+  rule.setAttribute("onclick","toggleDropdown(this)");
+
+  add(line);
+
+}
+
+function add(el) {
+  var list = document.getElementsByClassName("list");
+  list[0].appendChild(el);
+
+  window.location.hash = "#";
+
+  closeModal();
+}
+
+function toggleDropdown(el) {
+  el.classList.toggle("active");
+    var content = el.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+}
+
+function openModal() {
+  document.getElementsByClassName("modal")[0].style.display = "block";
+}
+
+function closeModal() {
+  var start = document.getElementById("start");
+  start.style.display = "block";
+  var end = document.getElementsByClassName("end");
+  for(var i = 0; i < end.length; i++)
+    end[i].style.display = "none";
+
+  document.getElementsByClassName("modal")[0].style.display = "none";
+  document.getElementById("newRuleForm").reset();
+}
+
+function removeMember(el) {
+  /*var payments = JSON.parse(localStorage.getItem("payments"));
+  var title = el.parentNode.previousSibling.innerHTML;
+  var index = payments.indexOf(title);
+  payments.splice(index, 2);
+  localStorage.setItem("payments", JSON.stringify(payments));*/
+
+  el.parentNode.parentNode.parentNode.removeChild(el.parentNode.parentNode);
+}
